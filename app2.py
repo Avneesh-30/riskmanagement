b6f5326354f990aaacde313f8b9ed33a27bcf390
Avneesh-30 <Avneesh.kumar@2centscapital.com>
2025-05-06 13:31:26 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -246,9 +246,8 @@ def sector_allocation(holdings_daily, prices_df):
     if sector_alloc.empty:
         st.warning("No sector allocation data available.")
     else:
+        st.bar_chart(sector_hist)
         st.pyplot(sector_alloc.plot.pie(autopct="%1.1f%%", figsize=(6, 6)).figure)
-    st.write("### Sector Allocation Table")
-    st.dataframe(sector_hist.sort_values(ascending=False).to_frame("Market Value"))
 
 # -------------- Trade History ---------------------
 
